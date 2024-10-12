8dae1260d90bf85a8c0e9cd8c36b809d4d44306f
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-12 21:28:51 +0600

    README.md updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite/plugin";
+import daisyui from "daisyui";
+import tailwindcssanimated from "tailwindcss-animated";
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 6s ease infinite alternate",
+      },
+    },
   },
-  plugins: [],
-}
+
+  plugins: [daisyui, flowbite, tailwindcssanimated],
+  daisyui: {
+    themes: ["light", "dark",],
+  },
+};
