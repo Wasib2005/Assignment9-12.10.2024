4ae6f08f9f170c7115866c7ea9a59944671e5589
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-13 23:29:29 +0600

    SomeOfOurHouses completed

---
+++ src/Components/Others/SomeOfTheHouses/SomeOfOurHouses.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
+import { getRandomItem } from "../../../utilities/GetRandomItem";
+
+
+const SomeOfOurHouses = ({ houseData }) => {
+  const [someHouseData, setSomeHouseData] = useState([]);
+  useEffect(() => {
+    setSomeHouseData(getRandomItem(houseData, 6));
+  }, [houseData]);
+  return (
+    <div className="">
+      <div className="text-center mt-20 lg:mt-44">
+        <h1 className=" text-xl lg:text-6xl font-bold">Some of Our Houses</h1>
+      </div>
+    
+    </div>
+  );
+};
+
+SomeOfOurHouses.propTypes = {
+    houseData: PropTypes.array
+};
+export default SomeOfOurHouses;
+
+;
