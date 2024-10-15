7bc86bf1a3c07c0fabb5fc0ed6c0d8ec1a56a66c
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-15 15:30:01 +0600

    House Comparison part Completed

--- src/Pages/HouseComparison.jsx
+++ src/Pages/HouseComparison.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { takeFromLS } from "../utilities/LS";
-import { useLoaderData } from "react-router-dom";
-import HouseShortInformation from "../Components/Others/houseShortInformation";
+import { Link, useLoaderData } from "react-router-dom";
 
 const HouseComparison = () => {
   const houseData = useLoaderData();
@@ -27,13 +26,93 @@ const HouseComparison = () => {
     );
   }
   return (
-    <div className="text-center mt-24">
+    <div className="text-center mt-24 grid gap-6 justify-center mb-28">
       <h1 className="font-bold text-2xl md:text-3xl lg:text-5xl">{`Here is your ${compareData.length} houses information in one place`}</h1>
-      <div className="flex flex-grow justify-around">
-        {compareData.map((house) => (
-          <HouseShortInformation key={house.id} house={house} />
-        ))}
-      </div>
+
+      <table className="table-fixed text-left border border-separate border-slate-500 text-white p-2">
+        <thead className="border bg-slate-500">
+          <tr>
+            {compareData.map((house) => (
+              <th className="p-2" key={house.id}>
+                {" "}
+                House No.{house.id}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-slate-400">
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                {house.segment_name}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                {house.small_description || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Total Room: {house.totalRooms || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Total Bed Room: {house.bedrooms || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Total Bath Room: {house.bathrooms || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Square Footage: {house.squareFootage || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Rent: {house.rent || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Build Date: {house.buildDate || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                Available From: {house.availableFrom || "-"}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {compareData.map((house) => (
+              <th className="  p-2" key={house.id}>
+                <Link to={`/PlacesProperties/${house.id}`} className="underline underline-offset-2 hover:underline-offset-4">Click here for more information</Link>
+              </th>
+            ))}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
